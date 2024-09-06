3d9aa5b91f53c4df794a4fe97a9fd8da403a2c57
Darth_neket <nikitacherenkov228@gmail.com>
2024-09-06 11:29:13 +0300

    Add modal card

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
 const API_URL = "https://mcuapi.herokuapp.com/api/v1/movies?page=1&limit=100";
-const SECOND_API_URL = "https://mcuapi.herokuapp.com/api/v1/tvshows?page=1&limit=100"
 const API_SEARCH = "https://mcuapi.herokuapp.com/api/v1/movies?page=1&limit=10&filter="
-const SECOND_API_SEARCH = "https://mcuapi.herokuapp.com/api/v1/tvshows?page=1&limit=10&filter="
 document.querySelector(".films").innerHTML = "";
 getFilms(API_URL);
-getFilms(SECOND_API_URL);
 
 async function getFilms(url){
     const response = await fetch(url, {
@@ -37,6 +34,7 @@ function showFilms(data) {
     <div class="film__phase">${"Phase: "+film.phase}</div>
   </div>
     `;
+        filmEl.addEventListener("click", () => openModal(film.title))
         filmsEl.appendChild(filmEl);
     });
 }
@@ -51,18 +49,15 @@ form.addEventListener("input", (e) => {
     clearTimeout(timeoutId);
 
     const apiSearchUrl = `${API_SEARCH}title=${search.value}`;
-    const apiSearchUrlSec = `${SECOND_API_SEARCH}title=${search.value}`;
 
     timeoutId = setTimeout(() => {
         if (search.value) {
             document.querySelector(".films").innerHTML = "";
 
             getFilms(apiSearchUrl);
-            getFilms(apiSearchUrlSec);
         } else {
             document.querySelector(".films").innerHTML = "";
             getFilms(API_URL);
-            getFilms(SECOND_API_URL);
         }
     }, 300);
 });
@@ -83,4 +78,53 @@ window.addEventListener('scroll', function() {
 
     lastScrollTop = scrollTop; // Обновляем последнее положение прокрутки
 });
+
+// Modal
+const modalEl = document.querySelector(".modal");
+
+async function openModal(title) {
+    const resp = await fetch(`${API_SEARCH}title=${title}`, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+    const respData = await resp.json();
+
+    modalEl.classList.add("modal--show");
+    document.body.classList.add("stop-scrolling");
+    console.log(respData[0])
+    modalEl.innerHTML = `
+    <div class="modal__card">
+      <img class="modal__movie-backdrop" src="${respData.data[0].cover_url}" alt="">
+      <h2>
+        <span class="modal__movie-title">${respData.data[0].title}</span>
+      </h2>
+      <ul class="modal__movie-info">
+        <div class="loader"></div>
+        ${respData.data[0].duration ? `<li class="modal__movie-runtime">Duration - ${respData.data[0].duration} min</li>` : ''}
+        <li class="modal__movie-overview">Description - ${respData.data[0].overview}</li>
+        <li class="modal__movie-overview">Date - ${respData.data[0].release_date}</li>
+      </ul>
+      <button type="button" class="modal__button-close">Закрыть</button>
+    </div>
+  `;
+    const btnClose = document.querySelector(".modal__button-close");
+    btnClose.addEventListener("click", () => closeModal());
+}
+
+function closeModal() {
+    modalEl.classList.remove("modal--show");
+    document.body.classList.remove("stop-scrolling");
+}
+
+window.addEventListener("click", (e) => {
+    if (e.target === modalEl) {
+        closeModal();
+    }
+})
+window.addEventListener("keydown", (e) => {
+    if (e.keyCode === 27) {
+        closeModal();
+    }
+})
 
